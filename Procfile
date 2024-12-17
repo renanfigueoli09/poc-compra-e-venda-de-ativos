7522171ef91a65a67795a4408d426bc1490936a8
renanfigueoli09 <renanfigueoli@outlook.com>
2024-12-17 01:04:22 -0300

    config celery

--- Procfile
+++ Procfile
@@ -2,4 +2,4 @@ api_flask: flask run --host=0.0.0.0 --port=$PORT
 
 celery_worker: celery -A app.celery worker --loglevel=info
 schedule: celery -A app.celery beat --loglevel=DEBUG 
-flower: celery flower -A app.celery  --break=$CELERY_BROKER_ENDPOINT --port=$PORT
+flower: celery flower -A app.celery --port=$PORT
--- src/config/celery_config.py
+++ src/config/celery_config.py
@@ -1,4 +1,5 @@
 import os
+import re
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -6,42 +7,49 @@ from celery import Celery
 from redis.sentinel import Sentinel
 
 
+def parse_sentinel_endpoint(endpoint):
+    match = re.search(
+        r"^(?P<prefix>sentinel\:\/\/(.+@)?).*(?P<suffix>\/service_name\:.+)$", endpoint
+    )
+    if match and match.group("prefix") and match.group("suffix"):
+        servers = endpoint.split(",")
+        for i in range(0, len(servers)):
+            # Add prefix when not founded
+            if not servers[i].startswith(match.group("prefix")):
+                servers[i] = match.group("prefix") + servers[i]
+            # Remove suffix when founded
+            if servers[i].endswith(match.group("suffix")):
+                servers[i] = servers[i].replace(match.group("suffix"), "")
+        broker_url = ";".join(servers)
+        return broker_url
+
+
 def make_celery(app):
     DBAAS_SENTINEL_SERVICE_NAME = os.getenv("DBAAS_SENTINEL_SERVICE_NAME")
-    DBAAS_SENTINEL_HOSTS: str | None = os.getenv("DBAAS_SENTINEL_HOSTS")
-    host = DBAAS_SENTINEL_HOSTS.split(",")
-    print(host)
-    DBAAS_SENTINEL_PORT = os.getenv("DBAAS_SENTINEL_PORT")
-    DBAAS_SENTINEL_PASSWORD = os.getenv("DBAAS_SENTINEL_PASSWORD")
-    DBAAS_MONGODB_ENDPOINT = os.getenv("DBAAS_MONGODB_ENDPOINT")
-    CELERY_BROKER_ENDPOINT = os.getenv("CELERY_BROKER_ENDPOINT")
-    # url = f"sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[0]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[1]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[2]}:{DBAAS_SENTINEL_PORT}/0"
+    # DBAAS_MONGODB_ENDPOINT = os.getenv("DBAAS_MONGODB_ENDPOINT")
+    DBAAS_SENTINEL_ENDPOINT = os.getenv("DBAAS_SENTINEL_ENDPOINT")
+    # url = f"sentinel://{DBAAS_SENTINEL_PASSWsimORD}@{host[0]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[1]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[2]}:{DBAAS_SENTINEL_PORT}/0"
     # url = f"sentinel://{host[0]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{host[1]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{host[2]}:{DBAAS_SENTINEL_PORT}/0"
-    print("TASDASFADSFGADSF")
-    print(CELERY_BROKER_ENDPOINT)
-
     # sentinel = Sentinel([('sentinel1', 26379), ('sentinel2', 26379), ('sentinel3', 26379)], socket_timeout=0.1)
     # master = sentinel.master_for(DBAAS_SENTINEL_SERVICE_NAME, socket_timeout=0.1)
     # print(master.connection_pool.get_connection())
-    url = "redis://172.21.0.5:26379,172.21.0.6:26380?sentinel=redis_master&password=mypassword"
+    print("tr6srcghcct")
+    print(DBAAS_SENTINEL_ENDPOINT)
+    print(DBAAS_SENTINEL_SERVICE_NAME)
+    sentinel_urls = parse_sentinel_endpoint(DBAAS_SENTINEL_ENDPOINT)
+    # sentinel_urls = "sentinel://sentinel1:26379/0"
+    print(sentinel_urls)
     celery = Celery(
         app.import_name,
-        # broker_url = f'redis://{redis_master.connection_pool.get_connection().host}:{redis_master.connection_pool.get_connection().port}/0',
-        # result_backend = f'redis://{redis_master.connection_pool.get_connection().host}:{redis_master.connection_pool.get_connection().port}/0',
-        # redis_sentinel_service = 'redis_master',
         broker_transport_options={
-            # "sentinels": [
-            #     ("sentinel1", 26379),
-            #     ("sentinel2", 26380),
-            #     ("sentinel3", 26381),
-            # ],
-            "master_name": "redis_master",  # Configuração do nome do mestre no Redis Sentinel
-            "socket_timeout": 0.1,
-            "sentinel_kwargs": {"password": ""},
+            "master_name": DBAAS_SENTINEL_SERVICE_NAME,
         },
-        # broker="sentinel://172.21.0.5:26379/0",
-        # broker=url,
-        backend=url,
+        broker=sentinel_urls,
+        # backend=sentinel_urls,
+        # result_backend=sentinel_urls,
+        # result_backend_transport_options={
+        #     "master_name": DBAAS_SENTINEL_SERVICE_NAME,
+        # },
         enable_utc=True,
         timezone="America/Sao_Paulo",
         task_serializer="json",
@@ -53,11 +61,11 @@ def make_celery(app):
             "request_id=%(request_id)s message=%(message)s"
         ),
         date_time_format="%Y-%m-%dT%H:%M:%S%z",
-        alternative_result_backend=DBAAS_MONGODB_ENDPOINT,
-        celery_max_retries=7,
-        result_expires=0,
-        result_persistent=True,
-        MONGO_PORT="27017",
+        # alternative_result_backend=DBAAS_MONGODB_ENDPOINT,
+        # celery_max_retries=7,
+        # result_expires=0,
+        # result_persistent=True,
+        # MONGO_PORT="27017",
     )
     celery.conf.update(app.config)
     return celery
