b63fa34ac826e62e42996c6e0174eb267f39d9d0
renanfigueoli09 <renanfigueoli@outlook.com>
2024-12-17 15:37:29 -0300

    config sentinels urls

--- src/config/celery_config.py
+++ src/config/celery_config.py
@@ -25,31 +25,26 @@ def parse_sentinel_endpoint(endpoint):
 
 
 def make_celery(app):
-    DBAAS_SENTINEL_SERVICE_NAME = os.getenv("DBAAS_SENTINEL_SERVICE_NAME")
-    # DBAAS_MONGODB_ENDPOINT = os.getenv("DBAAS_MONGODB_ENDPOINT")
+    DBAAS_SENTINEL_HOSTS = os.getenv("DBAAS_SENTINEL_HOSTS")
+    DBAAS_SENTINEL_PORT = os.getenv("DBAAS_SENTINEL_PORT")
     DBAAS_SENTINEL_ENDPOINT = os.getenv("DBAAS_SENTINEL_ENDPOINT")
-    # url = f"sentinel://{DBAAS_SENTINEL_PASSWsimORD}@{host[0]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[1]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{DBAAS_SENTINEL_PASSWORD}@{host[2]}:{DBAAS_SENTINEL_PORT}/0"
-    # url = f"sentinel://{host[0]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{host[1]}:{DBAAS_SENTINEL_PORT}/0;sentinel://{host[2]}:{DBAAS_SENTINEL_PORT}/0"
-    # sentinel = Sentinel([('sentinel1', 26379), ('sentinel2', 26379), ('sentinel3', 26379)], socket_timeout=0.1)
-    # master = sentinel.master_for(DBAAS_SENTINEL_SERVICE_NAME, socket_timeout=0.1)
-    # print(master.connection_pool.get_connection())
+    DBAAS_SENTINEL_PASSWORD = os.getenv("DBAAS_SENTINEL_PASSWORD")
     print("tr6srcghcct")
     print(DBAAS_SENTINEL_ENDPOINT)
-    print(DBAAS_SENTINEL_SERVICE_NAME)
-    sentinel_urls = parse_sentinel_endpoint(DBAAS_SENTINEL_ENDPOINT)
-    # sentinel_urls = "sentinel://sentinel1:26379/0"
+    sentinel_hosts_list = [
+        (host, DBAAS_SENTINEL_PORT) for host in DBAAS_SENTINEL_HOSTS.split(",")
+    ]
+    sentinel = Sentinel(
+        sentinels=sentinel_hosts_list,
+        socket_timeout=0.1,
+        password=DBAAS_SENTINEL_PASSWORD,
+    )
+    master = sentinel.discover_master("mymaster")
+    sentinel_urls = f"redis://{master[0]}:{master[1]}/0"
     print(sentinel_urls)
     celery = Celery(
-        app.import_name,
-        broker_transport_options={
-            "master_name": DBAAS_SENTINEL_SERVICE_NAME,
-        },
         broker=sentinel_urls,
-        # backend=sentinel_urls,
-        # result_backend=sentinel_urls,
-        # result_backend_transport_options={
-        #     "master_name": DBAAS_SENTINEL_SERVICE_NAME,
-        # },
+        backend=sentinel_urls,
         enable_utc=True,
         timezone="America/Sao_Paulo",
         task_serializer="json",
@@ -61,11 +56,13 @@ def make_celery(app):
             "request_id=%(request_id)s message=%(message)s"
         ),
         date_time_format="%Y-%m-%dT%H:%M:%S%z",
-        # alternative_result_backend=DBAAS_MONGODB_ENDPOINT,
-        # celery_max_retries=7,
-        # result_expires=0,
-        # result_persistent=True,
-        # MONGO_PORT="27017",
     )
-    celery.conf.update(app.config)
+
+    # celery.conf.update(app.config)
+    class ContextTask(celery.Task):
+        def __call__(self, *args, **kwargs):
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery.Task = ContextTask
     return celery
